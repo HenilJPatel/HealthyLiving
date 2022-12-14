c134c49f0eb3d258dd112d7c5377b6f5dfa473dd
HenilJPatel <hjpatel@student.fullsail.edu>
2022-12-14 03:24:10 -0500

    Product Page

--- app/src/main/java/com/example/healthyliving/ProductListLoad.java
+++ app/src/main/java/com/example/healthyliving/ProductListLoad.java
@@ -38,17 +38,16 @@ public class ProductListLoad extends RecyclerView.Adapter<ProductListLoad.MyView
 
     @Override
     public void onBindViewHolder(@NonNull ProductListLoad.MyViewHolder holder, int position) {
-        ProductData pres= list.get(position);
+        ProductData prod= list.get(position);
         {
-            DatabaseReference db= FirebaseDatabase.getInstance().getReference("Product/"+pres.getpID());
+            DatabaseReference db= FirebaseDatabase.getInstance().getReference("Product/"+prod.getpID());
             db.addListenerForSingleValueEvent(new ValueEventListener() {
                 @Override
                 public void onDataChange(@NonNull DataSnapshot snapshot) {
                     try {
                         String msg = snapshot.child("pName").getValue(String.class);
                         holder.Productname.setText(msg);
-                        String brand = snapshot.child("pBrand").getValue(String.class);
-                        holder.brand.setText(brand);
+
                         String url=snapshot.child("pURL").getValue(String.class);
                         Picasso.with(context).load(url).into(holder.img);
                         holder.img.setImageResource(R.drawable.noimage);
@@ -76,8 +75,8 @@ public class ProductListLoad extends RecyclerView.Adapter<ProductListLoad.MyView
         ImageView img;
         public MyViewHolder(@NonNull View itemView) {
             super(itemView);
-            img=itemView.findViewById(R.id.imageView123);
-            Productname=itemView.findViewById(R.id.productname);
+            img=itemView.findViewById(R.id.Prod_Image);
+            Productname=itemView.findViewById(R.id.Prod_ProductName);
             /*brand=itemView.findViewById(R.id.brandname);
             daysupply=itemView.findViewById(R.id.daySupply);
             noofrefill=itemView.findViewById(R.id.Refill);
@@ -86,15 +85,12 @@ public class ProductListLoad extends RecyclerView.Adapter<ProductListLoad.MyView
             nextrefill=itemView.findViewById(R.id.NextRefill);*/
 
             context=itemView.getContext();
-            itemView.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    int itemPosition  = getLayoutPosition();
-                    /*Toast.makeText(context, "" + list.get(itemPosition).getPresID(), Toast.LENGTH_SHORT).show();
-                    Intent intent = new Intent(context,PrescriptionDetails_Activity.class);
-                    intent.putExtra("presID",list.get(itemPosition).getPresID().toString());
-                    context.startActivity(intent);*/
-                }
+            itemView.setOnClickListener(v -> {
+                int itemPosition  = getLayoutPosition();
+                /*Toast.makeText(context, "" + list.get(itemPosition).getPresID(), Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(context,PrescriptionDetails_Activity.class);
+                intent.putExtra("presID",list.get(itemPosition).getPresID().toString());
+                context.startActivity(intent);*/
             });
         }
     }
