07a381282fcec38a2f5d8c2d8a01358fd9608209
HenilJPatel <hjpatel@student.fullsail.edu>
2022-12-14 00:17:58 -0500

    Product Page

--- app/src/main/java/com/example/healthyliving/Product_Page.java
+++ app/src/main/java/com/example/healthyliving/Product_Page.java
@@ -2,14 +2,54 @@ package com.example.healthyliving;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
 
 public class Product_Page extends AppCompatActivity {
+    RecyclerView recyclerView;
+    DatabaseReference databaseReference;
+    PrescriptionLoadAdapter prescriptionLoadAdapter;
+    ArrayList<PrescriptionData> list;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_product_page);
-
+        setContentView(R.layout.activity_prescription_list);
+        recyclerView=findViewById(R.id.PrescriptionViewer);
+        databaseReference= FirebaseDatabase.getInstance().getReference("Product");
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        list=new ArrayList<>();
+        prescriptionLoadAdapter = new PrescriptionLoadAdapter(this,list);
+        recyclerView.setAdapter(prescriptionLoadAdapter);
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for (DataSnapshot dataSnapshot:snapshot.getChildren()){
+                    try {
+                        PrescriptionData data = dataSnapshot.getValue(PrescriptionData.class);
+                        list.add(data);
+                    } catch (Exception e){
+                        System.out.println(e.toString());
+                    }
+                }
+                if(list.size()>0){
+                    prescriptionLoadAdapter.notifyDataSetChanged();
+                }
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+            }
+        });
     }
 }
