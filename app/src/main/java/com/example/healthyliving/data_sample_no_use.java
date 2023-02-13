944efc206105f685b9a862ffc591c5b80d90f112
HenilJPatel <hjpatel@student.fullsail.edu>
2023-02-13 10:02:52 -0500

    OpenFDA Api setup in Product Section

---
+++ app/src/main/java/com/example/healthyliving/data_sample_no_use.java
@@ -0,0 +1,215 @@
+package com.example.healthyliving;
+
+public class data_sample_no_use {
+    /*{"effective_time": "20210902",
+            "inactive_ingredient": [
+        "INACTIVE INGREDIENTS Sucrose"
+      ],
+        "purpose": [
+        "USES USES: Temporary Relief - Acne, Boils* * Claims based on traditional homeopathic practice, not accepted medical evidence. Not FDA evaluated."
+      ],
+        "keep_out_of_reach_of_children": [
+        "Keep this and all medication out of reach of children"
+      ],
+        "warnings": [
+        "WARNINGS This product is to be used for self-limiting conditions If symptoms do not improve in 4 days, or worsen, discontinue use and seek assistance of health professional. As with any drug, if you are pregnant, or nursing a baby, seek professional advice before taking this product. Keep this and all medication out of reach of children Do not use if capseal is broken or missing. Close the cap tightly after use."
+      ],
+        "questions": [
+        "QUESTIONS OR COMMENTS www.Rxhomeo.com | 1.888.2796642 | info@rxhomeo.com Rxhomeo, Inc 3200 Commander Dr, Ste 100-W1, Carrollton, TX 75006 USA"
+      ],
+        "spl_product_data_elements": [
+        "SILICEA SILICEA SUCROSE SILICON DIOXIDE SILICON DIOXIDE"
+      ],
+        "openfda": {
+        "brand_name": [
+        "SILICEA"
+        ],
+        "generic_name": [
+        "SILICEA"
+        ],
+        "manufacturer_name": [
+        "Rxhomeo Private Limited d.b.a. Rxhomeo, Inc"
+        ],
+        "product_ndc": [
+        "15631-0404"
+        ],
+        "product_type": [
+        "HUMAN OTC DRUG"
+        ],
+        "route": [
+        "ORAL"
+        ],
+        "substance_name": [
+        "SILICON DIOXIDE"
+        ],
+        "spl_id": [
+        "ca7bbcc8-2354-375c-e053-2995a90a72a0"
+        ],
+        "spl_set_id": [
+        "0000025c-6dbf-4af7-a741-5cbacaed519a"
+        ],
+        "package_ndc": [
+        "15631-0404-0",
+                "15631-0404-1",
+                "15631-0404-2",
+                "15631-0404-3",
+                "15631-0404-4",
+                "15631-0404-5",
+                "15631-0404-6",
+                "15631-0404-7"
+        ],
+        "is_original_packager": [
+        true
+        ],
+        "upc": [
+        "8907460005526"
+        ],
+        "unii": [
+        "ETJ7Z6XBU4"
+        ]
+    },
+        "version": "2",
+            "dosage_and_administration": [
+        "DOSAGE Adults- Take 4 or 6 Pellets by mouth, three times daily or as suggested by physician. Children 2 years and older- take 1/2 the adult dose."
+      ],
+        "pregnancy_or_breast_feeding": [
+        "As with any drug, if you are pregnant, or nursing a baby, seek professional advice before taking this product."
+      ],
+        "stop_use": [
+        "If symptoms do not improve in 4 days, or worsen, discontinue use and seek assistance of health professional."
+      ],
+        "storage_and_handling": [
+        "STORAGE Store in a cool dark place"
+      ],
+        "do_not_use": [
+        "Do not use if capseal is broken or missing. Close the cap tightly after use."
+      ],
+        "package_label_principal_display_panel": [
+        "Mini-Label Label-Pellets Blister-Pack Carton-Pack"
+      ],
+     -   "indications_and_usage": [
+        "INDICATIONS Condition listed above or as directed by the physician"
+      ],
+    -    "set_id": "0000025c-6dbf-4af7-a741-5cbacaed519a",
+   -         "id": "ca7bbcc8-2354-375c-e053-2995a90a72a0",
+  -          "active_ingredient": [
+        "ACTIVE INGREDIENT SILICEA HPUS 2X and higher"
+      ]
+    },
+    {
+        "effective_time": "20150109",
+            "inactive_ingredient": [
+        "INGREDIENTS: TALC, POLYMETHYL METHACRYLATE, VINYL DIMETHICONE/METHICONE SILSESQUIOXANE CROSSPOLYMER, CALCIUM SILICATE, TRIETHYLHEXANOIN, ALUMINUM HYDROXIDE, LAUROYL LYSINE, METHICONE, PHENOXYETHANOL, DIMETHICONE, ALUMINUM DIMYRISTATE, HYDROXYAPATITIE [+/-: MICA (CI77019), IRON OXIDES (CI 77491/CI 77492/CI 77499)]"
+      ],
+        "purpose": [
+        "Purpose Sunscreen"
+      ],
+        "keep_out_of_reach_of_children": [
+        "Keep out of reach of children If product is swallowed, get medical help or contact a Poison Control Center right away"
+      ],
+        "warnings": [
+        "Warnings For external use only."
+      ],
+        "when_using": [
+        "When using this product keep out of eyes. Rinse with water to remove."
+      ],
+        "spl_product_data_elements": [
+        "CHANTECAILLE PROTECTION NATURELLE BRONZE SPF 46 TITANIUM DIOXIDE, OCTINOXATE, ZINC OXIDE TITANIUM DIOXIDE TITANIUM DIOXIDE OCTINOXATE OCTINOXATE ZINC OXIDE ZINC OXIDE TALC CALCIUM SILICATE TRIETHYLHEXANOIN ALUMINUM HYDROXIDE LAUROYL LYSINE PHENOXYETHANOL DIMETHICONE ALUMINUM DIMYRISTATE MICA FERRIC OXIDE RED"
+      ],
+        "openfda": {},
+        "version": "4",
+            "dosage_and_administration": [
+        "Directions Protection Naturelle SPF 46 PA+++ Powder can be used on clean skin or over makeup. Shake lightly to activate the flow of powder. Sweep the brush all over the face to evenly distribute powder for immediate UVA/UVB protection."
+      ],
+        "package_label_principal_display_panel": [
+        "CHANTECAILLE Protection Naturelle SPF 46 PA+++ Powder NET WT. 0.088 OZ. 2.5g e",
+                "CHANTECAILLE PROTECTION NATURELLE BRONZE SPF 46 .088OZ/2.5g (42893-030-00) CHANTECAILLE PROTECTION NATURELLE SPF 46"
+      ],
+        "indications_and_usage": [
+        "Uses Multi-purpose mineral powder provides broad-spectrum SPF 46 PA+++ protection. Leaves the skin flawless and protected."
+      ],
+        "set_id": "0000076a-fc39-4208-ace8-6c2cb367904f",
+            "id": "f229e866-5775-4e42-a316-8480dd92fec6",
+            "active_ingredient": [
+        "BRONZE ACTIVE INGREDIENTS: TITANIUM DIOXIDE 2 %, ETHYLHEXYL METHOXYCINNAMATE 7%, ZINC OXIDE 24.5%"
+      ]
+    },
+    {
+        "effective_time": "20180801",
+            "inactive_ingredient": [
+        "INACTIVE INGREDIENTS Sucrose/Lactose"
+      ],
+        "purpose": [
+        "USES To relieve the symptoms of itching."
+      ],
+        "keep_out_of_reach_of_children": [
+        "KEEP OUT OF REACH OF CHILDREN Keep this and all medicines out of reach of children."
+      ],
+        "warnings": [
+        "STOP USE AND ASK DOCTOR If symptoms persist/worsen or if pregnant/nursing, stop use and consult your practitioner."
+      ],
+        "spl_product_data_elements": [
+        "Mezereum DAPHNE MEZEREUM BARK SUCROSE LACTOSE DAPHNE MEZEREUM BARK DAPHNE MEZEREUM BARK white"
+      ],
+        "openfda": {
+        "brand_name": [
+        "Mezereum"
+        ],
+        "generic_name": [
+        "DAPHNE MEZEREUM BARK"
+        ],
+        "manufacturer_name": [
+        "Washington Homeopathic Products"
+        ],
+        "product_ndc": [
+        "68428-511"
+        ],
+        "product_type": [
+        "HUMAN OTC DRUG"
+        ],
+        "route": [
+        "ORAL"
+        ],
+        "substance_name": [
+        "DAPHNE MEZEREUM BARK"
+        ],
+        "spl_id": [
+        "75d8c3b8-9025-aa00-e053-2a91aa0a0c66"
+        ],
+        "spl_set_id": [
+        "00006ebc-ec2b-406c-96b7-a3cc422e933f"
+        ],
+        "package_ndc": [
+        "68428-511-03",
+                "68428-511-05",
+                "68428-511-11",
+                "68428-511-12",
+                "68428-511-06"
+        ],
+        "is_original_packager": [
+        true
+        ],
+        "upc": [
+        "0740640391006"
+        ],
+        "unii": [
+        "X2N6E405GV"
+        ]
+    },
+        "version": "2",
+            "dosage_and_administration": [
+        "DIRECTIONS Adults: Dissolve 3 to 5 under the tongue three times a day or as directed by Lic. Practitioner. Take at greater intervals as condition subsides. Children: Dissolve 3 to 5 under the tongue three times a day or as directed by Lic. Practitioner. Take at greater intervals as condition subsides."
+      ],
+        "package_label_principal_display_panel": [
+        "PRINCIPAL DISPLAY PANEL The OTC potency range of MEZEREUM is 2x–30x, 1c–30c, 200c, 1m, 10m, 50m, and CM. Availability is subject to change. All WHP single remedies are made to order; thus, the labels are printed on the same label stock as the orders are filled. ‘Bottle Size’ and ‘Potency’ vary on the label depending on customer choice. Standard bottle sizes for pellet-form remedies are 2 dram, 4 dram, 1 ounce, 2 ounce, and 4 ounce. Label"
+      ],
+        "indications_and_usage": [
+        "INDICATIONS Indications: MEZEREUM Itching"
+      ],
+        "set_id": "00006ebc-ec2b-406c-96b7-a3cc422e933f",
+            "id": "75d8c3b8-9025-aa00-e053-2a91aa0a0c66",
+            "active_ingredient": [
+        "ACTIVE INGREDIENTS MEZEREUM"
+      ]
+    }*/
+}
